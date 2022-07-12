936852bd7f9baf3d7096e02b82b1e969c1204167
Javier Solis <javsolis@mit.edu>
2022-07-12 12:21:22 -0500

    Seperated CSS and JS into their own files

---
+++ main.js
@@ -0,0 +1,10 @@
+function addTimestamp(){
+    if (Date.parse(document.lastModified) != 0){                        
+        const div = document.getElementById('lastModified');
+        div.insertAdjacentHTML('afterbegin', "<p>Last Modified " + document.lastModified + " EST</p>");
+    }
+}
+        
+window.addEventListener('load', function() {
+    addTimestamp();
+})
